31194029002dec8fbda560dd3e6318dfb0a06f8c
Michel Max <michel.max@education.lu>
2026-02-04 15:44:23 +0100

    end lof lp3

--- static/form.js
+++ static/form.js
@@ -18,3 +18,24 @@ document.getElementById('submitButton').addEventListener('click', async (event) 
         .then(console.log("success"))
         .catch(errorMSG => console.error("error " + errorMSG));
 });
+
+// if i feel this is too complicated, how else can i approach this entire year??
+document.getElementById('showAllUsers').addEventListener('click', async (event) => {
+    fetch('/api/get_all_users')
+        .then(result => result.json())
+        .then(result => {
+            let table = document.getElementById("bodyRows")
+            table.innerHTML = '';    // let's clear the table first
+            result.forEach(user => {
+                let newRow = document.createElement("tr");
+                newRow.innerHTML = `
+                    <td>${user.id}</td> 
+                    <td>${user.first_name}</td> 
+                    <td>${user.last_name}</td> 
+                    <td>${user.email}</td> 
+                `;
+                table.append(newRow);
+            });
+        })
+        .catch(err => console.error(err));
+})
